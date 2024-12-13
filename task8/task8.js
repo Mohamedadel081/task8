80ff2d8b49e66cb6407cfbefbfc8b4748df04313
Mohamedadel081 <145723620+Mohamedadel081@users.noreply.github.com>
2024-12-13 19:38:50 +0200

    Add files via upload

---
+++ task8/task8.js
@@ -0,0 +1,21 @@
+var num1 =+prompt("enter the frist number")
+var num2 = +prompt("enter the second number")
+
+sum=num1+num2;
+document.write("<h2>" +num1+ " + " +num2 +" = "+ sum + "</h2>");
+
+
+sub=num1-num2;
+document.write("<h2>"  +num1+ " - " +num2 + " = "  + sub + "</h2>");
+
+x = num1 / num2
+document.writeln("<h2>"  +num1+ " / " +num2 + " = "  + x +"</h2>" );
+
+
+y=num1%num2;
+document.write("<h2>"   +num1+ " % " +num2 + " = "  + y + "</h2> " );
+
+z = num1 ** num2;
+document.write("<h2>"  +num1+ " power " +num2 + " = "  + z + "</h2> ");
+
+
